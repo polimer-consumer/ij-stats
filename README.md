39101f0fa1fd001f3ad393fe817cc70d1f86c27b
Evgenii <jenyhlevnoy@gmail.com>
2024-11-04 23:25:19 +0200

    Updated gradle tasks, add Readme, add test build pipeline for github

---
+++ README.md
@@ -0,0 +1,21 @@
+# Methods list
+
+<!-- Plugin description -->
+**This plugin provides a list of all methods in the current file.**
+
+With this plugin, you can easily view a list of methods along with
+access modifiers, parameter types, and return types.
+Each method name is clickable, allowing to navigate to the method definition within the file.
+<!-- Plugin description end -->
+
+## Installation
+
+Clone the repository and run `./gradlew build`.
+Go into *build/distributions* and install it manually from this folder using
+<kbd>Settings/Preferences</kbd> > <kbd>Plugins</kbd> > <kbd>⚙️</kbd> > <kbd>Install plugin from disk...</kbd>
+
+## Usage
+
+After installation, the plugin will be available in a side panel as
+*Methods*. Simply open some class/file, and the list of methods in that file will be displayed.
+Click on any method in the list to navigate directly to its definition.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+import org.jetbrains.changelog.markdownToHTML
+
+fun properties(key: String) = providers.gradleProperty(key)
+fun environment(key: String) = providers.environmentVariable(key)
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.changelog") version "2.2.1"
 }
 
 group = "com.polimerconsumer"
@@ -18,6 +24,12 @@ intellij {
     plugins.set(listOf("org.jetbrains.kotlin"))
 }
 
+intellij {
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
+    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+}
 
 dependencies {
     compileOnly("org.jetbrains.kotlin:kotlin-compiler:2.0.0") // Kotlin PSI
@@ -32,5 +44,34 @@ tasks {
 
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion").get()
+    }
+
+    patchPluginXml {
+        version = properties("pluginVersion")
+        sinceBuild = properties("pluginSinceBuild")
+        untilBuild = properties("pluginUntilBuild")
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            with (it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+    }
+
+    signPlugin {
+        certificateChain = environment("CERTIFICATE_CHAIN")
+        privateKey = environment("PRIVATE_KEY")
+        password = environment("PRIVATE_KEY_PASSWORD")
     }
 }
